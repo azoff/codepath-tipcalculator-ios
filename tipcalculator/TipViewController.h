a9a4e4c5adf0e3237df2c26db9df415362bb783e
Jonathan Azoff <jon@azoffdesign.com>
2014-02-17 19:03:59 -1000

    first view

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jonathan Azoff on 2/17/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
